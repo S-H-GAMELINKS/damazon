12bbb694be53d84725ebd3b1cfd56d238c1b9129
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-23 17:39:03 +0900

    add carts for user

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,20 @@
 class CartsController < ApplicationController
+  before_action :set_product
+
   def index
   end
 
   def create
+    if current_user.carts.empty?
+      @cart = current_user.carts.build
+      @cart.save!
+    else
+      @cart = current_user.carts.last
+    end
+    @cart.add(@product, @product.price)
+    @cart.save
+
+    redirect_to @product
   end
 
   def edit
@@ -13,4 +25,9 @@ class CartsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def set_product
+      @product = Product.find(params[:product_id])
+    end
 end
