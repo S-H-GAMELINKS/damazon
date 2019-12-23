39b6f674e7cca1a31608d3d1352464f0c26570cd
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-23 15:41:44 +0900

    add Cloumns to Users

---
+++ db/migrate/20191223064111_add_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :postal_code, :string
+    add_column :users, :address, :text
+  end
+end
