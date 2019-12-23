9bcda4eb440469e8427aa0df276b4ff64c35969b
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-23 11:23:24 +0900

    fix reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'reviews/create'
-  get 'reviews/update'
-  get 'reviews/destroy'
   devise_for :admin_users
   devise_for :users
   resources :products
   resources :categories
+  resources :reviews, only: [:create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
