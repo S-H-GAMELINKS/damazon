d834e1044115e6c388ca1752520f1f6c19e60127
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-23 13:06:38 +0900

    using kaminari pagenation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+    PRODUCTS_PER = 12
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.page(params[:page]).per(PRODUCTS_PER)
   end
 
   # GET /products/1
