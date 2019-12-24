7a495d1c409b3129b0d10bcfb4147d3482208c50
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-24 17:30:09 +0900

    update cart items quantity

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,7 +1,20 @@
 class CartItemsController < ApplicationController
+  before_action :set_cart_item, only: [:update, :destroy]
+
   def update
+    @cart_item.update(cart_item_params)
+    redirect_to carts_path
   end
 
   def destroy
   end
+
+  private
+    def set_cart_item
+      @cart_item = CartItem.find(params[:id])
+    end
+
+    def cart_item_params
+      params.require(:cart_item).permit(:quantity)
+    end
 end
