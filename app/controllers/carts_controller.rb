b34efc9c14aba2b99f0262f758cd614085912964
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-12-24 10:30:20 +0900

    add check cart is done?

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,7 +5,7 @@ class CartsController < ApplicationController
   end
 
   def create
-    if current_user.carts.empty?
+    if current_user.carts.empty? or current_user.carts.last.done
       @cart = current_user.carts.build
       @cart.save!
     else
