fd6b4bd8ceab37003166997d10f06a14bfa8efe4
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-16 13:06:17 +0900

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_23_083924) do
+ActiveRecord::Schema.define(version: 2020_01_16_040240) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2019_12_23_083924) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "carriage", default: false, null: false
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
