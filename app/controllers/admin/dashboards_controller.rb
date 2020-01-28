c7701b45f03f8007802e154c4e40e7db63e5c40c
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-28 13:45:46 +0900

    Show This Month profit

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -6,8 +6,14 @@ module Admin
 
     def index
         today_buy_cart = Cart.where(done: true, updated_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day)
-        @today_profit = today_buy_cart.map{|cart|
+        @today_profit = today_buy_cart.sum{|cart|
             cart.cart_items.sum{|i| (i[:quantity] * i[:price_cents])}
+        }
+
+        @month_buy_cart = Cart.where(done: true, updated_at: Time.zone.now.all_month)
+
+        @month_profit = @month_buy_cart.sum{|cart|
+          cart.cart_items.sum{|i| (i[:quantity] * i[:price_cents]) }
         }
     end
   end
