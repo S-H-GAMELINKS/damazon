e2adbdcad1c9171331923928533ad3dbe448593e
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-28 21:16:07 +0900

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_16_065143) do
+ActiveRecord::Schema.define(version: 2020_01_28_121516) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 2020_01_16_065143) do
     t.text "address"
     t.string "phone"
     t.boolean "admin", default: false, null: false
+    t.string "token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
